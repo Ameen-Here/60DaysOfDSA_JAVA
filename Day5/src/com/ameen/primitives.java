0486146d3785cdb6ee6cb5b24d21ec305f7679e2
Ameen Noushad <86822695+Ameen-Here@users.noreply.github.com>
2022-02-15 18:54:10 +0530

    Day5
    Printing and reading data

---
+++ Day5/src/com/ameen/primitives.java
@@ -0,0 +1,14 @@
+package com.ameen;
+
+import java.util.Scanner;
+
+public class primitives {
+    public static void main(String[] args) {
+        System.out.println();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please Enter your roll number:");
+        int rollNo = input.nextInt();
+        System.out.println("Your roll number is "+ rollNo);
+    }
+}
+
